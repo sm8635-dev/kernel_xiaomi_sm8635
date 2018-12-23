30d3594ac190287888b5ba2a06c7c0d1a97a45d3
kdrag0n <dragon@khronodragon.com>
2018-12-22 22:36:19 -0800

    block: Disable I/O stats accounting by default
    While Android userspace (e.g. storaged) does use iostats via
    /proc/diskstats, init will explicitly enable iostats for the devices on
    which it is primarily used - sda and sdf. Avoid the 0.5-1% overhead for
    block devices that do not need it.
    
    Change-Id: Ic8a1896285c9169bd9fb6f0425b26ab4a6189e0f

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -602,8 +602,7 @@ struct request_queue {
 #define QUEUE_FLAG_NOWAIT       29	/* device supports NOWAIT */
 #define QUEUE_FLAG_SQ_SCHED     30	/* single queue style io dispatch */
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1UL << QUEUE_FLAG_IO_STAT) |		\
-				 (1UL << QUEUE_FLAG_SAME_COMP) |	\
+#define QUEUE_FLAG_MQ_DEFAULT	((1UL << QUEUE_FLAG_SAME_COMP) |		\
 				 (1UL << QUEUE_FLAG_NOWAIT))
 
 void blk_queue_flag_set(unsigned int flag, struct request_queue *q);
