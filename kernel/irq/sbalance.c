2c65b375b6dc11f3582c0b2424152e77bb4c070a
John Galt <johngaltfirstrun@gmail.com>
2025-03-22 11:13:32 -0400

    sbalance: Update del_timer_sync
    Change-Id: I9719818cf2f849de888b83192505a9c2

--- kernel/irq/sbalance.c
+++ kernel/irq/sbalance.c
@@ -375,7 +375,7 @@ static void sbalance_wait(long poll_jiffies)
 	timer.timer.expires = jiffies + poll_jiffies;
 	add_timer(&timer.timer);
 	schedule();
-	del_singleshot_timer_sync(&timer.timer);
+	del_timer_sync(&timer.timer);
 	destroy_timer_on_stack(&timer.timer);
 }
 
