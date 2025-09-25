f08dbc94b08ef34954aee06d04a1f05938d1ed57
John Galt <johngaltfirstrun@gmail.com>
2025-09-25 12:29:40 -0400

    cpu_boosting: Add kick support, decrease app launch boost duration

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -5255,7 +5255,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	if (!ret && !threadgroup &&
 	    !memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 	    task_is_zygote(task->parent)) {
-		cpu_boost_all(1000);
+		cpu_boost_max(500);
 	}
 
 out_finish:
