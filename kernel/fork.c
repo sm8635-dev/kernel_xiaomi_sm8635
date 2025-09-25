98deea9d0d85cf41df912873a5adf6d68909456b
John Galt <johngaltfirstrun@gmail.com>
2025-09-25 12:29:40 -0400

    cpu_boosting: Add kick support, decrease app launch boost duration

--- kernel/fork.c
+++ kernel/fork.c
@@ -2872,7 +2872,7 @@ pid_t kernel_clone(struct kernel_clone_args *args)
 
 	/* Boost CPUs to the max for 100 ms when userspace launches an app */
 	if (task_is_zygote(current))
-		cpu_boost_all(100);
+		cpu_boost_max(100);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
