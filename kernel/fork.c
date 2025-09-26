7b4c24356c3da9422c397030e1c102e947ec0f81
John Galt <johngaltfirstrun@gmail.com>
2025-09-26 07:55:48 -0400

    treewide: Add kprofiles support

--- kernel/fork.c
+++ kernel/fork.c
@@ -2839,6 +2839,8 @@ struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node)
 	return copy_process(NULL, 0, node, &args);
 }
 
+extern int kp_active_mode(void);
+
 /*
  *  Ok, this is the main fork-routine.
  *
@@ -2871,7 +2873,7 @@ pid_t kernel_clone(struct kernel_clone_args *args)
 		return -EINVAL;
 
 	/* Boost CPUs to the max for 100 ms when userspace launches an app */
-	if (task_is_zygote(current))
+	if (task_is_zygote(current) && kp_active_mode() != 1)
 		cpu_boost_max(100);
 
 	/*
