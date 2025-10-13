19f76e727b5a9cc667a77732100296938c22b5e6
John Galt <johngaltfirstrun@gmail.com>
2025-10-13 11:55:22 -0400

    launch boost: Decrease launch boost duration on kprofile 2

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -532,7 +532,10 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
 	if (!ret && !threadgroup &&
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 		task_is_zygote(task->parent) && kp_active_mode() != 1) {
-		cpu_boost_max(500);
+		if (kp_active_mode() == 3)
+			cpu_boost_max(500);
+		else
+			cpu_boost_max(250);
 	}
 
 out_finish:
